54ebce46d24132b863c311ca0f0133ebc3746517
sroy3333 <121767970+sroy3333@users.noreply.github.com>
2024-03-18 12:52:01 +0530

    Update commentRoutes.js

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,13 +1,9 @@
+// routes/commentRoutes.js
 const express = require('express');
 const router = express.Router();
 const commentController = require('../controllers/commentController');
 
 // Create a new comment
 router.post('/', commentController.createComment);
-
-// Delete a comment
-router.delete('/:commentId', commentController.deleteComment);
-
-// Implement other routes for fetching, updating comments
 
 module.exports = router;
